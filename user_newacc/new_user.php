4cb4ae634edb50a6e453cabe9f7c0912d60238e8
Ken-Warner <ken.c.warner@gmail.com>
2022-01-27 12:46:10 -0500

    -Fixed new account variable issue

--- user_newacc/new_user.php
+++ user_newacc/new_user.php
@@ -46,6 +46,9 @@ if (isset($_REQUEST['newusername'])) {
 		$username = "";
 		$submit_form = false;
 	}
+	
+	//populate form fields with values that were still valid
+	$webpage->convert("USERNAME", $username);
 } else { $submit_form = false; }
 
 //check password for proper format
@@ -74,6 +77,9 @@ if (isset($_REQUEST['email_address'])) {
 		$err_msg = "Invalid Email Address";
 	} else {
 		$email_address = $_REQUEST['email_address'];
+		
+		//populate form fields with values that were still valid
+		$webpage->convert("EMAIL_ADDRESS", $email_address);
 	}
 } else { $submit_form = false; }
 
@@ -104,10 +110,6 @@ if (($submit_form) && ($password == $p_confirm)) {
 } else {
 	//give feedback to the user about what went wrong
 	$webpage->convert("ERR_MSG", "<font style='color:red; font-size: 1em; padding-left: 1em;'>{$err_msg}</font>");
-	
-	//populate form fields with values that were still valid
-	$webpage->convert("USERNAME", $username);
-	$webpage->convert("EMAIL_ADDRESS", $email_address);
 }
 
 // Output webpage
