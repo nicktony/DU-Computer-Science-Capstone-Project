d4924c13dea34658d5fdd85d94ffe5efd3f5a1fe
nicktony <whitenick1219@gmail.com>
2022-02-09 23:32:00 -0500

    Fixing bug with arrow animations
    Started tweaking animation for day arrow buttons. Still working on the alignment of the arrows

--- schedule/schedule.php
+++ schedule/schedule.php
@@ -1,5 +1,19 @@
 <?php
 
+// DB Information
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Taskless";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 // Set default time zone and current date variables
 date_default_timezone_set('America/New_York');
 
@@ -144,8 +158,63 @@ $firstDayOfMonth = $year . '-' . $month . '-01';
 $dayOfWeek = date('w', strtotime($firstDayOfMonth));
 //echo '......................................' .$dayOfWeek;
 for ($i = 1; $i <= $maxDate; $i++) {
+	$dayTasks = "<div class='embeddedtask'>";
+	$tempDate = $year . '-' . $month . '-' . $i;
+
+	// Query for tasks
+	$sql = "SELECT title, description, start_date, priority, is_complete FROM tasks WHERE start_date = '$tempDate'";
+	$result = $conn->query($sql);
+
+  // Output data of each row
+  while($row = $result->fetch_assoc()) {
+  	$title = $row['title'];
+  	$description = $row['description'];
+  	$start_date = $row['start_date'];
+  	$priority = $row['priority'];
+  	$is_complete = $row['is_complete'];
+
+    $dayTasks .= "<div>$title</div>";
+  }
+  $dayTasks .= "</div>";
+
+	// Set style of day
 	if ($i > $dayOfWeek) {
-		if ($i == date('d') && $month == date('m') && $year == date('Y')) {
+		if ($i == date('d') && $month == date('m') && $year == date('Y') && $i == $day) {
+			$html .= "
+			<td class='currentday'>
+				<div id='day$i' class='dayiconactive'>
+					<div class='daylogoactive'>
+		        <div class='option-linking'>
+		        	<span class='linking-text daylogoactive-text' style='font-size: 20px;'>$i</span>
+							<svg
+		            aria-hidden='true'
+		            focusable='false'
+		            data-prefix='fad'
+		            data-icon='angle-double-right'
+		            role='img'
+		            xmlns='http://www.w3.org/2000/svg'
+		            viewBox='0 0 448 512'
+		            class='svg-inline--fa fa-angle-double-right fa-w-14 fa-5x'>
+		          
+		          <g class='fa-group'>
+								<path
+									fill='currentColor'
+									d='M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z'
+									class='fa-secondary'>
+								</path>
+								<path
+									fill='currentColor'
+									d='M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z'
+									class='fa-third'>
+								</path>
+		          </g>
+		          </svg>
+		        </div>
+			    </div>
+		  	</div>
+		  	$dayTasks
+			</td>";
+		} else if ($i == date('d') && $month == date('m') && $year == date('Y')) {
 			$html .= "
 			<td class='currentday'>
 				<div id='day$i' class='dayicon'>
@@ -178,14 +247,15 @@ for ($i = 1; $i <= $maxDate; $i++) {
 		        </div>
 			    </div>
 		  	</div>
+		  	$dayTasks
 			</td>";
 		} else if ($i == $day) {
 			$html .= "
 			<td class='selectedday'>
-				<div id='day$i' class='dayicon'>
-					<div class='daylogo'>
+				<div id='day$i' class='dayiconactive'>
+					<div class='daylogoactive'>
 		        <div class='option-linking'>
-		        	<span class='linking-text daylogo-text' style='font-size: 20px;'>$i</span>
+		        	<span class='linking-text daylogoactive-text' style='font-size: 20px;'>$i</span>
 							<svg
 		            aria-hidden='true'
 		            focusable='false'
@@ -212,6 +282,7 @@ for ($i = 1; $i <= $maxDate; $i++) {
 		        </div>
 			    </div>
 		  	</div>
+		  	$dayTasks
 			</td>";
 		} else {
 			$html .= "
@@ -246,6 +317,7 @@ for ($i = 1; $i <= $maxDate; $i++) {
 		        </div>
 			    </div>
 		  	</div>
+		  	$dayTasks
 			</td>";
 		}
 
@@ -259,32 +331,21 @@ for ($i = 1; $i <= $maxDate; $i++) {
 }
 $html .= "</tr></table>";
 
-// DB Information
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Taskless";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 // Query tasks for selected day
 $html .= "<table class='tasks'>";
-$sql = "SELECT name, description FROM tasks WHERE start_time = '$date'";
+$sql = "SELECT title, description, start_date, priority, is_complete FROM tasks WHERE start_date = '$date'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // Output data of each row
   while($row = $result->fetch_assoc()) {
-  	$task = $row['name'];
-  	$desc = $row['description'];
+  	$title = $row['title'];
+  	$description = $row['description'];
+  	$start_date = $row['start_date'];
+  	$priority = $row['priority'];
+  	$is_complete = $row['is_complete'];
 
-    $html .= "<tr><td><b>$desc</b>: $task</td></tr>";
+    $html .= "<tr><td><b>$title</b>: $description</td></tr>";
   }
 } else {
   //echo "0 results";
