f300114efb9bacf356bcb7e561ab88e243c9f2dd
Govinda Raj <79052277+GovindaG07@users.noreply.github.com>
2022-02-15 18:13:51 -0500

    Updated the email part on Support form.
    Added submit button to send message form . Updated the email part on support page.

--- support/support.php
+++ support/support.php
@@ -1,22 +1,28 @@
 <?php
-if (isset($_POST['submit'])){
+
+if (isset($_POST['submit']))
+{
     $name = $_POST['firstName'];
     $name =$_POST['lastName'];
     $phone =$_POST['phone'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
-
-    $email_form = 'capstone@gmail.com';
+    $email_from = 'capstone@gmail.com';
     $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
+    $email_body = "User Name: $name $name.\n".
               "User Email: $visitor_email.\n".
               "User Message: $message.\n";
-              $to = "graazg7@gmail.com";
+    $to = "graazg7@gmail.com";
 
   $headers="From: $email_from\r\n";
   $headers .= "Reply-To: $visitor_email\r\n";
-  mail($to, $email_subject,$email_body,$headers);
+  if(mail($to, $email_subject,$email_body,$headers))
+  {
+    header("Location: support.php? mailsend");
+  }
 }
+
+
 
 // Required files
 require '../classes/webpage.class.php';
@@ -29,13 +35,6 @@ $webpage->createPage('Support');
 
 // Assign body contents
 $html = file_get_contents('./contact.html');
-
-
-
-
-
-
-
 
 // Input additional css
 $webpage->inputCSS('./support.css');
